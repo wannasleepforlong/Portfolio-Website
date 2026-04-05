f763f0d4933d17a7a00eb1f41ce07829b1c83dff
wannasleepforlong <abhinavpainuli221b@gmail.com>
2026-04-06 02:18:12 +0530

    testing redoplyment in netifly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Testing Copyright © {year} SB</h3>
+          <h3>Copyright © {year} SB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
